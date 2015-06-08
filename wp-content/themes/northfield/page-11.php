34fdaf439ae3c0ebd780835507c6672fb5412973
Brad Sawicki <git@bradsawicki.com>
2015-06-07 23:56:59 -0500

    Contact page styles / Contact icons / Map img

--- wp-content/themes/northfield/page-11.php
+++ wp-content/themes/northfield/page-11.php
@@ -7,6 +7,32 @@
   	  
   	    <section class="main-content">
       		<h1 class="page-title"><?php the_title(); ?></h1>
+  		    <ul class="contact-info mobile-visible">
+            <li>
+              <div class="icon">
+                <svg class="icon-marker"><use xlink:href="#icon-marker" /></svg>
+              </div>
+              <?php the_field('contact_address'); ?>
+            </li>
+            <li>
+              <div class="icon">
+                <svg class="icon-phone"><use xlink:href="#icon-phone" /></svg>
+              </div>
+              <?php the_field('contact_phone'); ?>
+            </li>
+            <li>
+              <div class="icon">
+                <svg class="icon-fax"><use xlink:href="#icon-fax" /></svg>
+              </div>
+              <?php the_field('contact_fax'); ?>
+            </li>
+            <li>
+              <div class="icon">
+                <svg class="icon-mail"><use xlink:href="#icon-mail" /></svg>
+              </div>
+              <a href="mailto:<?php the_field('contact_email'); ?>"><?php the_field('contact_email'); ?></a>
+            </li>
+          </ul>
           <div class="body">
         		<?php the_content(); ?>
           </div>
@@ -15,14 +41,35 @@
         <aside id="sidebar" class="sidebar-right">
         
           <ul>
-            <li class="widget widget-map">
+            <li class="widget widget-map mobile-hidden">
+              <a href="https://goo.gl/maps/zw5bn" target="_blank"><img src="/wp-content/themes/northfield/dist/images/map.png" alt="Northfield Industries Location"></a>
             </li>
-            <li class="widget widget-contact-info">
-              <ul>
-                <li><?php the_field('contact_address'); ?></li>
-                <li><?php the_field('contact_phone'); ?></li>
-                <li><?php the_field('contact_fax'); ?></li>
-                <li><?php the_field('contact_email'); ?></li>
+            <li class="widget widget-contact-info mobile-hidden">
+              <ul class="contact-info">
+                <li>
+                  <div class="icon">
+                    <svg class="icon-marker"><use xlink:href="#icon-marker" /></svg>
+                  </div>
+                  <?php the_field('contact_address'); ?>
+                </li>
+                <li>
+                  <div class="icon">
+                    <svg class="icon-phone"><use xlink:href="#icon-phone" /></svg>
+                  </div>
+                  <?php the_field('contact_phone'); ?>
+                </li>
+                <li>
+                  <div class="icon">
+                    <svg class="icon-fax"><use xlink:href="#icon-fax" /></svg>
+                  </div>
+                  <?php the_field('contact_fax'); ?>
+                </li>
+                <li>
+                  <div class="icon">
+                    <svg class="icon-mail"><use xlink:href="#icon-mail" /></svg>
+                  </div>
+                  <a href="mailto:<?php the_field('contact_email'); ?>" class="contact-email"><?php the_field('contact_email'); ?></a>
+                </li>
               </ul>
             </li>
             <li class="widget widget-catalog">
