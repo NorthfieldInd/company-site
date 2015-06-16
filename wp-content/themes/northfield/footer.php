26f03cf18d0ead5a1f17e916217cd7a84814663e
Brad Sawicki <git@bradsawicki.com>
2015-06-16 17:40:06 -0500

    Button hover styles / News solutions hero img / new logo svgs / Jason feedback updates

--- wp-content/themes/northfield/footer.php
+++ wp-content/themes/northfield/footer.php
@@ -6,7 +6,7 @@
         			<?php wp_nav_menu( array('menu'=>'2') ); ?>
           </nav>
           
-          <p class="footer-contact"><span class="footer-address">700 Wiley Farm Court,</span> Schaumburg, IL 60173 <span class="bullet">&bull;</span> <span class="footer-phone">Phone +1(847)755-0777</span> <span class="bullet">&bull;</span> <span class="footer-fax">Fax +1(847)755-0779</span> <span class="bullet">&bull;</span> Email <a href="mailto:koliver@northfieldind.com">koliver@northfieldind.com</a></p>
+          <p class="footer-contact"><span class="footer-address">700 Wiley Farm Court,</span> Schaumburg, IL 60173 <span class="bullet">&bull;</span> <span class="footer-phone">Phone (847)755-0777</span> <span class="bullet">&bull;</span> <span class="footer-fax">Fax (847)755-0779</span> <span class="bullet">&bull;</span> Email <a href="mailto:koliver@northfieldind.com">koliver@northfieldind.com</a></p>
           
           <p class="footer-copyright">&copy;<?php echo date('Y'); ?> Northfield Industries <span class="footer-credit"><a href="http://bradsawicki.com" target="_blank">Web Development</a>: Brad Sawicki</span> <span class="pipe">|</span> <span class="footer-credit">Web Design: Frohlichstein Inc.</span></p>
   
