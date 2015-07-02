e1d5da1db52507f36a7dcfb8c1d4665bacdd3806
Brad Sawicki <bradsawicki@Brads-MacBook-Pro.local>
2015-07-02 18:07:13 -0500

    Products machining hubs switch

--- wp-content/themes/northfield/page-7.php
+++ wp-content/themes/northfield/page-7.php
@@ -7,11 +7,11 @@
   			<h1><?php the_field('prod_hero_title'); ?></h1>
   	  </div>
   	</section>
-  	
+
   	<nav id="products-nav" class="products-nav">
   	  <ul>
-  	    <li><a href="#products-section-1">Hubs and Spindles <span class="caret">></span></a></li>
-  	    <li><a href="#products-section-2">Machining <span class="caret">></span></a></li>
+  	    <li><a href="#products-section-1">Machining <span class="caret">></span></a></li>
+        <li><a href="#products-section-2">Hubs and Spindles <span class="caret">></span></a></li>
   	    <li><a href="#products-section-3">Jacks <span class="caret">></span></a></li>
   	  </ul>
   	</nav>
@@ -19,13 +19,13 @@
   	<section id="products-section-1" class="products-section-1">
   	  <div class="table-row">
   			<div class="tablet-image tablet-visible">
-			    <img src="<?php echo get_template_directory_uri(); ?>/dist/images/products-hubs-spindles.jpg" alt="Hubs and Spindles">
+          <img src="<?php echo get_template_directory_uri(); ?>/dist/images/products-machining.jpg" alt="Machining">
   			</div>
   			<div class="col-1-2 col-content">
   			  <?php the_field('prod_content_section_1'); ?>
   			</div>
   			<div class="col-1-2 col-image tablet-hidden">
-			    <img src="<?php echo get_template_directory_uri(); ?>/dist/images/products-hubs-spindles.jpg" alt="Hubs and Spindles">
+          <img src="<?php echo get_template_directory_uri(); ?>/dist/images/products-machining.jpg" alt="Machining">
   			</div>
   	  </div>
   	</section>
@@ -33,13 +33,13 @@
   	<section id="products-section-2" class="products-section-2">
   	  <div class="table-row">
   			<div class="tablet-image tablet-visible">
-			    <img src="<?php echo get_template_directory_uri(); ?>/dist/images/products-machining.jpg" alt="Machining">
+          <img src="<?php echo get_template_directory_uri(); ?>/dist/images/products-hubs-spindles.jpg" alt="Hubs and Spindles">
   			</div>
   			<div class="col-1-2 col-content">
   			  <?php the_field('prod_content_section_2'); ?>
   			</div>
   			<div class="col-1-2 col-image tablet-hidden">
-			    <img src="<?php echo get_template_directory_uri(); ?>/dist/images/products-machining.jpg" alt="Machining">
+          <img src="<?php echo get_template_directory_uri(); ?>/dist/images/products-hubs-spindles.jpg" alt="Hubs and Spindles">
   			</div>
   	  </div>
   	</section>
