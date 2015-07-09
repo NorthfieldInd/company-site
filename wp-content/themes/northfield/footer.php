b27a1b351dfdd8c38e6b581b3bac1f2324758fd2
Brad Sawicki <bradsawicki@Brads-MacBook-Pro.local>
2015-07-09 11:24:33 -0500

    Contact fields in footer

--- wp-content/themes/northfield/footer.php
+++ wp-content/themes/northfield/footer.php
@@ -6,7 +6,7 @@
         			<?php wp_nav_menu( array('menu'=>'3') ); ?>
           </nav>
 
-          <p class="footer-contact"><span class="footer-address">700 Wiley Farm Court,</span> Schaumburg, IL 60173 <span class="bullet">&bull;</span> <span class="footer-phone">Phone (847) 755-0700</span> <span class="bullet">&bull;</span> <span class="footer-fax">Fax (847) 755-0779</span> <span class="bullet">&bull;</span> Email <a href="mailto:koliver@northfieldind.com">koliver@northfieldind.com</a></p>
+          <p class="footer-contact"><span class="footer-address">700 Wiley Farm Court,</span> Schaumburg, IL 60173 <span class="bullet">&bull;</span> <span class="footer-phone">Phone <?php the_field('contact_phone', 11); ?></span> <span class="bullet">&bull;</span> <span class="footer-fax">Fax <?php the_field('contact_fax', 11); ?></span> <span class="bullet">&bull;</span> Email <a href="mailto:<?php the_field('contact_email', 11); ?>"><?php the_field('contact_email', 11); ?></a></p>
 
           <p class="footer-sub-co">Please visit our subsidiary company for more services at <a href="http://www.dnmachining.com" target="_blank">dnmachining.com</a></p>
 
