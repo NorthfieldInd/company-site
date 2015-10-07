9fc3ca0d14cfe6450961e7bd0784753d26ec4a12
Brad Sawicki <git@bradsawicki.com>
2015-10-07 20:10:48 +0200

    Google Analytics code

--- wp-content/themes/northfield/header.php
+++ wp-content/themes/northfield/header.php
@@ -15,6 +15,14 @@
     <script>try{Typekit.load();}catch(e){}</script>
     <script type="text/javascript" src="http://fast.fonts.net/jsapi/c9f3d264-6f01-4b53-95af-fb374ed4a35d.js"></script>
     <?php wp_head(); ?>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+      ga('create', 'UA-68547120-1', 'auto');
+      ga('send', 'pageview');
+    </script>
   	<!--[if lt IE 9]>
     <script src="<?php echo get_template_directory_uri(); ?>/assets/js/respond.min.js"></script>
     <![endif]-->
